5077320ad3a80f221335b07557b3e72b65dc4b43
vahid <vahid.gshf@gmail.com>
2019-11-26 12:55:12 -0500

    add mutator and accessor methods

--- order/src/ClassUsingThis.java
+++ order/src/ClassUsingThis.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class ClassUsingThis
 {
     int length,width;
@@ -12,6 +13,26 @@ public class ClassUsingThis
     {
         this.length=length; //this tells which one is class level arg
         this.width=width;
+    }
+    Scanner input = new Scanner(System.in);
+//    mutator-set-input values
+    public void setLength(){
+        System.out.println("input length");
+        length=input.nextInt();
+    }
+    public void setWidth(){
+        System.out.println("input width");
+        width=input.nextInt();
+    }
+//    accessor methods- get - return output
+//    return type should match the variable datatype u wanna return
+    public int getLength()
+    {
+        return length;
+    }
+    public int getWidth()
+    {
+        return width;
     }
     int area()
     {
--- order/src/MainUsingThisClass.java
+++ order/src/MainUsingThisClass.java
@@ -3,9 +3,12 @@ public class MainUsingThisClass
     public static void main(String[] args) {
 //        creating object using default constructor
         ClassUsingThis obj1 = new ClassUsingThis();
-        System.out.println("Area using def cons" + obj1.area());
+        System.out.println("Area using def cons " + obj1.area());
+        obj1.setLength();
+        obj1.setWidth();
+        System.out.println("Area using def cons " + obj1.area());
 //        creating object using par. const.
         ClassUsingThis obj2 = new ClassUsingThis(3,4);
-        System.out.println("Area using def cons" + obj2.area());
+        System.out.println("Area using def cons " + obj2.area());
     }
 }
